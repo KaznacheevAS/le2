c9e9fdf4d1e1a94c7e7a53aae0016120642df100
KaznacheevA.S <kaznacheev.a.s@yandex.ru>
2022-01-05 21:02:12 +0300

    Initial Commit

---
+++ le2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  le2
+//
+//  Created by Anton Kaznacheev on 05.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
